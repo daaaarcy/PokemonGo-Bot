2e6d9acadb4e352b8f8cfff9266d9c46d2ebb1c2
Darcy Qiu <darcy.qiu@gmail.com>
2016-07-28 22:22:30 +0800

    Add fly distance

--- pokemongo_bot/cell_workers/move_to_fort_worker.py
+++ pokemongo_bot/cell_workers/move_to_fort_worker.py
@@ -3,6 +3,7 @@ from pokemongo_bot.human_behaviour import sleep
 from pokemongo_bot import logger
 
 class MoveToFortWorker(object):
+    FLY_DIST = 150
     def __init__(self, fort, bot):
         self.fort = fort
         self.api = bot.api
@@ -22,8 +23,8 @@ class MoveToFortWorker(object):
         logger.log('[#] Found fort {} at distance {}'.format(
             fortID, format_dist(dist, unit)))
 
-        if dist > 10:
-            logger.log('[#] Need to move closer to Pokestop')
+        if dist > self.FLY_DIST:
+            logger.log('[#] Need to move closer to Pokestop (' + str(dist) + 'm away)')
             position = (lat, lng, 0.0)
 
             if self.config.walk > 0:
@@ -34,7 +35,16 @@ class MoveToFortWorker(object):
             self.api.player_update(latitude=lat, longitude=lng)
             response_dict = self.api.call()
             logger.log('[#] Arrived at Pokestop')
-            sleep(2)
+            sleep(0.5)
             return response_dict
+        elif dist <= self.FLY_DIST and dist > 10:
+            # fly to the fort
+            self.api.player_update(latitude=lat, longitude=lng)
+            response_dict = self.api.call()
+            logger.log('[#] Flew to Pokestop (dist within ' + str(self.FLY_DIST) + 'm)')
+            sleep(0.5)
+            return response_dict
+        else:
+            logger.log('[#] Arrived at Pokestop (within in 10m)')
 
         return None
